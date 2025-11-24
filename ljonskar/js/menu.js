e5d705f9334d2355e3a83e72993a99b26c7f952d
JimJukx <jimmy.cattiau@gmail.com>
2025-11-24 16:14:35 +0100

    Create js/menu.js

---
+++ ljonskar/js/menu.js
@@ -0,0 +1,15 @@
+// ============================================
+// JS GLOBAL : MENU - Lag Spirit MC
+// ============================================
+
+// Ce fichier est lié à toutes les pages du site.
+// Il servira à contrôler les comportements du menu,
+// comme le menu responsive ou les effets visuels.
+
+/* Exemple futur :
+window.addEventListener("scroll", () => {
+  // Ajouter une ombre au menu quand on descend
+});
+*/
+
+// Pour l’instant, rien ici. À compléter selon les besoins.
