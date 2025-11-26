5f4770cca06217f35edb2e2bc5089bf327960ea5
JimJukx <jimmy.cattiau@gmail.com>
2025-11-26 14:14:33 +0100

    Update roles-handler.php

--- ljonskarwp/plugins/ljonskar-core/handlers/roles-handler.php
+++ ljonskarwp/plugins/ljonskar-core/handlers/roles-handler.php
@@ -1,39 +1,28 @@
 <?php
+if (!defined('ABSPATH')) exit;
 
-add_action('init', 'ljonskar_handle_roles_update');
+class LjonskarRolesHandler {
 
-function ljonskar_handle_roles_update() {
-
-    if (!isset($_POST['lj_action']) || $_POST['lj_action'] !== 'update_roles') {
-        return;
+    public static function init() {
+        add_shortcode('ljonskar_admin_roles', [__CLASS__, 'render_roles_dashboard']);
     }
 
-    if (!is_user_logged_in()) return;
+    public static function render_roles_dashboard() {
 
-    $current = wp_get_current_user();
-
-    if (!user_can($current, 'manage_ljonskar_roles')) return;
-
-    $user_id = intval($_POST['user_id']);
-    $caps = isset($_POST['caps']) ? $_POST['caps'] : [];
-
-    $all_caps = [
-        'manage_ljonskar_roles',
-        'manage_ljonskar_events',
-        'manage_ljonskar_gallery',
-        'manage_ljonskar_shop'
-    ];
-
-    $user = get_user_by('id', $user_id);
-
-    foreach ($all_caps as $cap) {
-        if (in_array($cap, $caps)) {
-            $user->add_cap($cap);
-        } else {
-            $user->remove_cap($cap);
+        if (!is_user_logged_in()) {
+            return "<p>Veuillez vous connecter.</p>";
         }
-    }
 
-    wp_redirect($_SERVER['HTTP_REFERER']);
-    exit;
+        $user = wp_get_current_user();
+
+        if (!in_array('ljonskar_roles_manager', $user->roles)) {
+            return "<h3>⛔ Accès refusé</h3>";
+        }
+
+        ob_start();
+        ljonskar_render_template('dashboard-roles');
+        return ob_get_clean();
+    }
 }
+
+LjonskarRolesHandler::init();
